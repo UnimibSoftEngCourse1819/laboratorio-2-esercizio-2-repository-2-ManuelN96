bcd0eca88fb7a64f1322a8441ed611493161eab6
ManuelN96 <44461602+ManuelN96@users.noreply.github.com>
2019-01-13 00:09:51 +0100

    Add files via upload

---
+++ lab2_es2/laboratorio2_es2/lab2_es2/src/lab2_es2/Class1.java
@@ -0,0 +1,18 @@
+package lab2_es2;
+
+public class Class1 {
+	public static void main(String[] args) {
+
+		 
+	}
+	
+	public void secondMethod(){
+		
+	}
+	
+	public void firstMethod() {
+		
+	}
+	
+}
+
